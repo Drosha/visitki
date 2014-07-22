48900e62b5e5b4add66e96ca2474fcf46d9dfa06
Drosha <drosha@gmail.com>
2014-07-22 17:48:34 +0300

    Added drop-down with backgrounds

--- js/app/modules.js
+++ js/app/modules.js
@@ -1,14 +1,16 @@
-var builderAppCtrl = angular.module('builderAppCtrl', []);
-
-angular.module('buttonDropDown', [])
+var builderAppCtrl = angular.module('builderAppCtrl', [])
     .directive('buttonDropDown', function() {
         return {
             restrict: 'E',
-            scope: {model: '=', options: '='},
-            controller: function () {},
+            scope: {model: '=', backgrounds: '='},
+            controller: function ($scope) {
+                $scope.change = function(option){
+                    $scope.model = option;
+                };
+            },
             template: '<button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" ng-model="bgItems">Выбрать готовый<span class="caret"></span></button>'+
-                '<ul class="dropdown-menu" role="menu">'+
-                '<li ng-repeat="background in backgrounds"><a href="#"><div class="bg-preview" style="background-color:{{background.value}}"></div>{{background.title}}</a></li>'+
-                '</ul>'
+             '<ul class="dropdown-menu" role="menu">'+
+             '<li ng-repeat="background in backgrounds" ng-click="change(background.value)"><a href="#"><div class="bg-preview" style="background-color:{{background.value}}"></div>{{background.label}}</a></li>'+
+             '</ul>'
         }
     });
